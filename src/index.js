d06db19d98145294efa7abce9292e7173fcb9ffc
Stanislav Kintoriak <kintoriak@gmail.com>
2023-04-01 19:24:03 +0300

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // imports
 import SimpleLightbox from '../node_modules/simplelightbox';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import GetPictures from './js/fetch-pictures';
 import './css/styles.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import throttle from 'lodash.throttle';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 // querySelectors
 const body = document.querySelector('body');
@@ -64,29 +64,29 @@ function onSubmitBtnClick(e) {
 function createMarkup(pictures) {
   const arrayOfPictures = pictures.data.hits;
 
+  // info massages code
 
-// info massages code
-
-if (arrayOfPictures.length === 0) {
+  if (arrayOfPictures.length === 0) {
     Notiflix.Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.'
-  );
-} else if (arrayOfPictures.length === 1) {
-  Notify.failure(`Hooray! We found ${pictures.data.hits} images.`);
-}
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  } else if (getPictures.page === 1) {
+    Notiflix.Notify.info(`Hooray! We found ${pictures.data.totalHits} images.`);
+  }
 
-const galleryMarkup = arrayOfPictures
-  .map(
-    ({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => {
-      return `
+  const galleryMarkup = arrayOfPictures
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
             <a class="link card-item" href="${largeImageURL}">
                 <div class="photo-card">
                     <img class="card-img" src="${webformatURL}" alt="${tags}" width="300" loading="lazy">
@@ -99,88 +99,78 @@ const galleryMarkup = arrayOfPictures
                 </div>
                 </div>
             </a>`;
-    }
-  )
-  .join('');
+      }
+    )
+    .join('');
 
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+  gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
+  //SIMPLELIGHTBOX
+  const lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
 
-
- //SIMPLELIGHTBOX
- const lightbox = new SimpleLightbox('.gallery a');
- lightbox.refresh();
- 
-
-if (getPictures.totalHits <= getPictures.perPage && arrayOfPictures.length !== 0) {
-
+  if (
+    getPictures.totalHits <= getPictures.perPage &&
+    arrayOfPictures.length !== 0
+  ) {
     noPictures();
     return;
-};
+  }
 
-function noPictures() {
-
-    Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);
-
-    if (Math.ceil(getPictures.totalHits / getPictures.perPage) === 1) {
-        return;
-    }
-
-    document.removeEventListener('scroll', onScroll);}
-
-
-if (getPictures.page <= Math.floor(getPictures.totalHits / getPictures.perPage)) {
-    loadMorePictures();
-};
-};
-
-
-
-function loadMorePictures() {
-
-    const onScroll = function() {
-        
-    const { height: cardHeight } = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-        top: cardHeight * 2 + 30,
-        behavior: 'smooth',
-    });
-
-        const {
-            scrollTop,
-            scrollHeight,
-            clientHeight
-        } = document.documentElement;
-        
-        if (scrollTop + clientHeight >= scrollHeight - 200) {
-            
-            getPictures.fetchPictures(searchQuery)
-                .then(createMarkup)
-                .catch(error => console.error(error));
-        };
-    };
-    
-    if (!isScrollListenerRegistered) {
-        isScrollListenerRegistered = true;
-        document.addEventListener('scroll', throttle(onScroll, 3000));
-    };
-};
-
-function noMorePictures() {
-
-    Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);
+  function noPictures() {
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
 
     if (Math.ceil(getPictures.totalHits / getPictures.perPage) === 1) {
-        return;
+      return;
     }
 
     document.removeEventListener('scroll', onScroll);
-};
+  }
 
+  if (
+    getPictures.page <= Math.floor(getPictures.totalHits / getPictures.perPage)
+  ) {
+    loadMorePictures();
+  }
+}
 
+function loadMorePictures() {
+  const onScroll = function () {
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
 
+    window.scrollBy({
+      top: cardHeight * 2 + 30,
+      behavior: 'smooth',
+    });
 
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
+    if (scrollTop + clientHeight >= scrollHeight - 200) {
+      getPictures
+        .fetchPictures(searchQuery)
+        .then(createMarkup)
+        .catch(error => console.error(error));
+    }
+  };
 
+  if (!isScrollListenerRegistered) {
+    isScrollListenerRegistered = true;
+    document.addEventListener('scroll', throttle(onScroll, 3000));
+  }
+}
 
+function noMorePictures() {
+  Notiflix.Notify.failure(
+    `We're sorry, but you've reached the end of search results.`
+  );
+
+  if (Math.ceil(getPictures.totalHits / getPictures.perPage) === 1) {
+    return;
+  }
+
+  document.removeEventListener('scroll', onScroll);
+}
